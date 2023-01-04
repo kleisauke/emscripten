c3e8f38faa8f98da47ea0ac2f519e31e029e945f
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-01-04 14:38:52 +0100

    Add support for dynamic modules on Deno

--- src/closure-externs/closure-externs.js
+++ src/closure-externs/closure-externs.js
@@ -267,3 +267,14 @@ Navigator.prototype.webkitGetUserMedia = function(
  * @type {symbol}
  */
 Symbol.dispose;
+
+/**
+ * Deno
+ */
+var Deno = {};
+
+/**
+ * @param {string|URL} path
+ * @return {Promise<Uint8Array>}
+ */
+Deno.readFile = function(path) {};
