0c88bb3d67a0639dbcf924fd486bb6b158f1cb50
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-02-25 13:02:30 +0100

    mimalloc: sync with upstream (microsoft/mimalloc@2cca58d)

--- system/lib/mimalloc/src/prim/emscripten/prim.c
+++ system/lib/mimalloc/src/prim/emscripten/prim.c
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------------
-Copyright (c) 2018-2023, Microsoft Research, Daan Leijen
+Copyright (c) 2018-2023, Microsoft Research, Daan Leijen, Alon Zakai
 This is free software; you can redistribute it and/or modify it under the
 terms of the MIT license. A copy of the license can be found in the file
 "LICENSE" at the root of this distribution.
@@ -249,4 +249,3 @@ void _mi_prim_thread_associate_default_heap(mi_heap_t* heap) {
 
 }
 #endif
-
